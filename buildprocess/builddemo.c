8cab4319a59496bad13d37f1b3ce439e317f9b9f
NEELISAIKIRAN <neelisaikiran1997@gmail.com>
2020-10-17 20:26:12 +0530

    Demo for make files

---
+++ buildprocess/builddemo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int sub(int, int);
+int add(int , int);
+int main(int argc, char const *argv[])
+{
+    printf("addition is %d\n",add(10,20));
+    printf("addition is %d\n",sub(40,20));
+    return 0;
+}
